64a433f698f1b5b56685f876fa0fd81b95a58a02
Sasha Chernyakov <sashachernyakov111111@gmail.com>
2026-08-21 22:12:25 +0400

    Fetch mod info from info.json

--- main.go
+++ main.go
@@ -19,19 +19,29 @@ type Repo struct {
 	ArchiveURL    string `json:"archive_url"`
 	DefaultBranch string `json:"default_branch"`
 }
+type ModInfo struct {
+	Name        string `json:"name"`
+	Version     string `json:"version"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+}
+
 type Mod struct {
-	Name        string
-	Description string
+	ModInfo
 	DownloadURL string `json:"download_url"`
 }
 
 func main() {
 	topic := "tlc-mod"
-	base_url := "https://api.github.com/search/repositories"
+	baseURL := "https://api.github.com/search/repositories"
+	rawContent, err := url.Parse("https://raw.githubusercontent.com")
+	if err != nil {
+		log.Fatal(err)
+	}
 	filename := "mods.json"
 	archiveFormat := "zipball"
 
-	u, err := url.Parse(base_url)
+	u, err := url.Parse(baseURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,23 +54,43 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer resp.Body.Close()
 	var data SearchResponse
 	err = json.NewDecoder(resp.Body).Decode(&data)
-	defer resp.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	repos := data.Items
 	log.Printf("Processing %d repos", len(repos))
-	mods := make([]Mod, len(repos))
-	for i, repo := range repos {
-		mods[i].Name = repo.Name
-		mods[i].Description = repo.Description
+	mods := make([]Mod, 0, len(repos))
+	for _, repo := range repos {
+		rawContent.Path = repo.FullName + "/refs/heads/" + repo.DefaultBranch + "/info.json"
+		log.Printf("Fetching %s", rawContent.String())
+		resp, err := http.Get(rawContent.String())
+		if err != nil {
+			log.Printf("Failed fetching: %s", err)
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Returned error: %s", resp.Status)
+			continue
+		}
+		var modInfo ModInfo
+		err = json.NewDecoder(resp.Body).Decode(&modInfo)
+		if err != nil {
+			log.Printf("Failed parsing: %s", err)
+			continue
+		}
+
+		var mod Mod
+		mod.ModInfo = modInfo
 		u := repo.ArchiveURL
 		u = strings.ReplaceAll(u, "{archive_format}", archiveFormat)
 		u = strings.ReplaceAll(u, "{/ref}", "/"+repo.DefaultBranch)
-		mods[i].DownloadURL = u
+		mod.DownloadURL = u
+		mods = append(mods, mod)
 		log.Printf("Added %s as mod", repo.FullName)
 	}
 	log.Printf("Saving %d mods to %s", len(mods), filename)
